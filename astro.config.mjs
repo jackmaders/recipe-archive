0f752068d8d65496f0853efe62154ccda4f4cb50
Jack Maders <jackwmaders@gmail.com>
2024-08-02 19:14:26 +0100

    feat: :sparkles: add recipe files

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,32 @@ export default defineConfig({
       social: {
         github: "https://github.com/withastro/starlight",
       },
+      sidebar: [
+        {
+          label: "Beef",
+          autogenerate: { directory: "beef" },
+        },
+        {
+          label: "Chicken",
+          autogenerate: { directory: "chicken" },
+        },
+        {
+          label: "Lamb",
+          autogenerate: { directory: "lamb" },
+        },
+        {
+          label: "Pork",
+          autogenerate: { directory: "pork" },
+        },
+        {
+          label: "Sea Food",
+          autogenerate: { directory: "seafood" },
+        },
+        {
+          label: "Vegetarian",
+          autogenerate: { directory: "vegetarian" },
+        },
+      ],
     }),
   ],
 });
